da7289752e3ea0f2a1c301a3bb8479bc2d63d61e
Kevin F. <kevinjuli@nforst.de>
2026-01-05 16:12:24 +0100

    Rework FourDigitCode to include toString(), equals() and hashCode()

--- backend/src/main/java/com/mycompany/app/App.java
+++ backend/src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,6 @@ public class App {
     public static void main(String[] args) {
         QRCodeGenerator qrCodeGenerator = new QRCodeGenerator("www.youtube.com");
         qrCodeGenerator.saveQRCode();
-        IO.println(new FourDigitCodeGenerator().newCode().getValueAsString());
+        IO.println(new FourDigitCodeGenerator().newCode());
     }
-
 }
--- backend/src/main/java/com/mycompany/app/FourDigitCodeGenerator.java
+++ backend/src/main/java/com/mycompany/app/FourDigitCodeGenerator.java
@@ -14,19 +14,37 @@ public class FourDigitCodeGenerator {
 
 		private final int value;
 
-		public String getValueAsString() {
-			return String.valueOf(value);
-		}
-
 		public int getValue() {
 			return value;
 		}
 
 		FourDigitCode(int value) {
 			if (value < 0 || value > 9999) {
-				throw new IllegalArgumentException("Invalid code value.");
+				throw new IllegalArgumentException("Invalid code value " + value + ".");
 			}
 			this.value = value;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%04d", value);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null || getClass() != obj.getClass()) {
+				return false;
+			}
+			FourDigitCode other = (FourDigitCode) obj;
+			return this.value == other.value;
+		}
+
+		@Override
+		public int hashCode() {
+			return Integer.hashCode(value);
 		}
 	}
 }
